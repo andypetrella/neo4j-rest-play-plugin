89623b9f7dbacccc4fea97ec15d223988c224a31
andy petrella <andy.petrella@gmail.com>
2012-04-17 22:10:03 +0200

    Publish snapshot to cloudbees

--- project/Build.scala
+++ project/Build.scala
@@ -1,9 +1,10 @@
 import sbt._
 import Keys._
 
+
 object MinimalBuild extends Build {
 
-  lazy val buildVersion =  "1.0-SNAPSHOT"
+  lazy val buildVersion =  "0.0.1-SNAPSHOT"
 
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
@@ -12,6 +13,14 @@ object MinimalBuild extends Build {
   val scalaz = "org.scalaz" %% "scalaz-core" % scalazVersionNumber withSources
 
   val specs2 = "org.specs2" %% "specs2" % "1.8.2" % "test" withSources
+
+  val snapshotRegex = ".*-(.+)$".r
+
+  val cloudbees = "https://repository-andy-petrella.forge.cloudbees.com/"
+  val cloudbeesRepo = buildVersion match {
+    case snapshotRegex(x) => x.toLowerCase at cloudbees + x.toLowerCase + "/"
+    case _ => throw new IllegalArgumentException("Bad version")
+  }
 
   val libDependencies = Seq(
     "play" %% "play" % "2.0",
@@ -23,12 +32,17 @@ object MinimalBuild extends Build {
   )
 
 
-
-  lazy val root = Project(id = "neo4j-rest-play-plugin", base = file("."), settings = Project.defaultSettings).settings(
-    version := buildVersion,
-    organization := "be.nextlab",
-    resolvers ++= Seq(typesafe, typesafeSnapshot),
-    javacOptions += "-Xlint:unchecked",
-    libraryDependencies ++= libDependencies
-  )
+  val cloudbeesCredentials = Credentials(file("project/cloudbees.credentials"))
+  lazy val root = {
+    Project(id = "neo4j-rest-play-plugin", base = file("."), settings = Project.defaultSettings).settings(
+      version := buildVersion,
+      organization := "be.nextlab",
+      resolvers ++= Seq(typesafe, typesafeSnapshot, cloudbeesRepo),
+      javacOptions += "-Xlint:unchecked",
+      libraryDependencies ++= libDependencies,
+      publishMavenStyle := true,
+      publishTo := Some(cloudbeesRepo),
+      credentials += cloudbeesCredentials
+    )
+  }
 }
