ca21bd1b947f0b7923f2d62603b6ee82f5d1163c
Andy Petrella <andy.petrella@gmail.com>
2012-09-24 21:21:51 +0200

    an attempt commented out that might be continued about implementing filter on VP

--- src/main/scala/be/nextlab/play/neo4j/rest/ValidationPromised.scala
+++ src/main/scala/be/nextlab/play/neo4j/rest/ValidationPromised.scala
@@ -7,13 +7,13 @@ import scalaz.Scalaz._
 case class ValidationPromised[E, A](promised: Promise[Validation[E, A]]) {
     import ValidationPromised._
 
-    def /~> [EE >: E, B](f: Validation[E, A] => ValidationPromised[EE, B]): ValidationPromised[EE, B] = 
-        promised flatMap { valid => 
+    def /~> [EE >: E, B](f: Validation[E, A] => ValidationPromised[EE, B]): ValidationPromised[EE, B] =
+        promised flatMap { valid =>
             f(valid).promised
         } transformer
 
-    def map[B](f: A => B): ValidationPromised[E, B] = 
-        promised map { valid => 
+    def map[B](f: A => B): ValidationPromised[E, B] =
+        promised map { valid =>
             valid.fold(
                 fail => KO(fail),
                 suc => OK(f(suc))
@@ -21,8 +21,8 @@ case class ValidationPromised[E, A](promised: Promise[Validation[E, A]]) {
         } transformer
 
 
-    def /~~>[EE >: E, B](f: A => ValidationPromised[EE, B]): ValidationPromised[EE, B] = 
-        promised flatMap { valid => 
+    def /~~>[EE >: E, B](f: A => ValidationPromised[EE, B]): ValidationPromised[EE, B] =
+        promised flatMap { valid =>
             valid.fold (
                 bad => Promise.pure(KO(bad)),
                 good => f(good).promised
@@ -36,6 +36,16 @@ case class ValidationPromised[E, A](promised: Promise[Validation[E, A]]) {
         s: A => B
     ) = promised.map(_.fold(f, s))
 
+    // E must be something like Pointed or alike to have this
+    //
+    // def filter(f:A=>Boolean):ValidationPromised[E, A] = flatMap{a =>
+    //     if (f(a)) {
+    //         Promise.pure(OK(a))
+    //     } else {
+    //         Promise.pure(KO("This ValidationPromised doens't pass the given filter : "))
+    //     }
+    // }
+
 }
 
 object ValidationPromised {
@@ -44,15 +54,15 @@ object ValidationPromised {
         def transformer:ValidationPromised[E, A] = ValidationPromised(promised)
     }
 
-    implicit def pimpPromiseValidation[E,A](p:Promise[Validation[E, A]]):PromiseValidationW[E, A] = 
+    implicit def pimpPromiseValidation[E,A](p:Promise[Validation[E, A]]):PromiseValidationW[E, A] =
         PromiseValidationW(p)
 
-    implicit def pimpValidationPromised[E,A](p:ValidationPromised[E, A]):Promise[Validation[E, A]] = 
+    implicit def pimpValidationPromised[E,A](p:ValidationPromised[E, A]):Promise[Validation[E, A]] =
         p.promised
 
     def pureSuccess[E, A](a:A):ValidationPromised[E, A] = Promise.pure(OK(a).asInstanceOf[Validation[E,A]]).transformer
 
-    def sequence[E,A](vs:Seq[ValidationPromised[E, A]]):ValidationPromised[E, Seq[A]] = 
+    def sequence[E,A](vs:Seq[ValidationPromised[E, A]]):ValidationPromised[E, Seq[A]] =
         vs.foldLeft(pureSuccess[E,Seq[A]](Seq():Seq[A])) {(acc, v) =>
             acc.flatMap{(xs:Seq[A]) => v.map {(vr:A) => (v +: xs).asInstanceOf[Seq[A]]}}
         }
